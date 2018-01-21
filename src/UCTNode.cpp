02d278cc0a7fd32dc7a48869425604e1c7364fe5
Andy Olsen <andy.olsen.tx@gmail.com>
2018-01-20 20:34:05 -0600

    Tune first play urgency.
    Change FPU urgency to be parent-0.2f instead of parent.
    
    Credit @Eddh, issue #696.

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -295,7 +295,7 @@ float UCTNode::get_eval(int tomove) const {
         if (tomove == FastBoard::WHITE) {
             eval = 1.0f - eval;
         }
-        return eval;
+        return eval - 0.2f;
     }
 }
 
